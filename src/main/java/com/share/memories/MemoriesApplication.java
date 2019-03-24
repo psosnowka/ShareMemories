0aae0f9e26bde82c5bdeca8d308f31bed366d427
psosnowka <pawel.sosnowka@upaid.pl>
2019-03-24 12:44:31 +0100

    Initial commit

---
+++ src/main/java/com/share/memories/MemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.share.memories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriesApplication.class, args);
+	}
+
+}
